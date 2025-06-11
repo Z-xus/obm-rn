3a915e4df9587b5a8d7b6d0bb53fe56fb3b343aa
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2025-06-11 15:59:52 +0530

    add buttons on the home screen

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { Link, Stack } from 'expo-router';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Button } from '~/components/Button';
+
+
+const routes = [
+  { name: '/(tabs)/check', label: 'Go to Check' },
+  { name: '/(tabs)/email', label: 'Go to Email' },
+  { name: '/(tabs)/phone', label: 'Go to Phone' },
+] as const;
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <ScrollView contentContainerStyle={styles.content}>
+        {routes.map(({ name, label }) => (
+          <Link key={name} href={name} asChild>
+            <Button title={label} />
+          </Link>
+        ))}
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  content: {
+    padding: 15,
+    gap: 12
+  }
+});
